2f58ae2c150c834618b1dcbe51bd668067309583
silloi <silloi1993@gmail.com>
2022-03-02 18:38:19 +0900

    feature: relate user to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
+  before_action :authenticate, only: [:new, :create, :update, :destroy]
 
   # GET /posts or /posts.json
   def index
@@ -22,7 +23,7 @@ class PostsController < ApplicationController
 
   # POST /posts or /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = Post.new(**post_params, user_id: current_user.id)
 
     respond_to do |format|
       if @post.save
